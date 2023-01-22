39ef047abce382271a283ea543e12e2f6ea8457a
Petro <vankevychpetro@gmai.com>
2023-01-23 00:39:25 +0200

    final commit

--- backend.php
+++ backend.php
@@ -6,58 +6,99 @@ header('Access-Control-Allow-Methods: *');
 
 $data = $_POST;
 
-fetch($data['address_1'], $data['address_2'], $data['city'], $data['state'], $data['zip-code']);
+    fetch($data['address_1'], $data['address_2'], $data['city'], $data['state'], $data['zip-code']);
 
-function fetch($address1, $address_2 = "", $city = "", $state = "", $zipCode = "")
-{
-    $input_data = '<AddressValidateRequest USERID="105SELF02197">
-<Revision>1</Revision>
-<Address ID="0">
-<Address1>' . "$address1" . '</Address1>
-<Address2>' . "$address_2" . '</Address2>
-<City>' . "$city" . '</City>
-<State>' . "$state" . '</State>
-<Zip5>' . "$zipCode" . '</Zip5>
-<Zip4></Zip4>
-</Address>
-</AddressValidateRequest>';
+    function fetch($address1, $address_2 = "", $city = "", $state = "", $zipCode = "")
+    {
+        var_dump($address1);
+        $input_data = '<AddressValidateRequest USERID="105SELF02197">
+        <Revision>1</Revision>
+        <Address ID="0">
+        <Address1>' . "$address1" . '</Address1>
+        <Address2>' . "$address_2" . '</Address2>
+        <City>' . "$city" . '</City>
+        <State>' . "$state" . '</State>
+        <Zip5>' . "$zipCode" . '</Zip5>
+        <Zip4></Zip4>
+        </Address>
+        </AddressValidateRequest>';
 
-    $fields = array(
-        'API' => 'Verify',
-        'XML' => $input_data
-    );
+        $fields = array(
+            'API' => 'Verify',
+            'XML' => $input_data
+        );
 
-    $url = 'http://production.shippingapis.com/ShippingAPITest.dll?' . http_build_query($fields);
+        $url = 'http://production.shippingapis.com/ShippingAPITest.dll?' . http_build_query($fields);
 
-    $curl = curl_init();
-    curl_setopt($curl, CURLOPT_URL, $url);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-    curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 300);
+        $curl = curl_init();
+        curl_setopt($curl, CURLOPT_URL, $url);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 300);
 
-    try {
-        $response = new SimpleXMLElement(curl_exec($curl));
-        $err = curl_error($curl);
+        try {
+            $response = new SimpleXMLElement(curl_exec($curl));
+            $err = curl_error($curl);
 
-        curl_close($curl);
+            curl_close($curl);
 
-        if ($err) {
-            echo "cURL Error #:" . $err;
-        } else {
-            $error = $response->Address->Error->Description;
-            if ($error) {
-                echo json_encode(['error' => (string) $error]);
+            if ($err) {
+                echo "cURL Error #:" . $err;
             } else {
-                echo json_encode([
-                    'usps_address_1' => (string) $response->Address->Address1,
-                    'usps_address_2' => (string) $response->Address->Address2,
-                    'usps_city' => (string) $response->Address->City,
-                    'usps_state' => (string) $response->Address->State,
-                    'usps_zip-code' => (string) $response->Address->Zip5]);
-            }
+                $error = $response->Address->Error->Description;
+                if ($error) {
+                    echo json_encode(['error' => (string)$error]);
+                } else {
+                    echo json_encode([
+                        'usps_address_1' => (string)$response->Address->Address1,
+                        'usps_address_2' => (string)$response->Address->Address2,
+                        'usps_city' => (string)$response->Address->City,
+                        'usps_state' => (string)$response->Address->State,
+                        'usps_zip-code' => (string)$response->Address->Zip5]);
+                }
 
+            }
+        } catch (Exception $e) {
+            echo "Exception";
+            echo $e;
         }
-    } catch (Exception $e) {
-        echo "Exception";
-        echo $e;
-    }
+}
+
+if ($_POST['save_data']) {
+    $host = 'localhost:3301';
+    $db = 'validation-form';
+    $user = 'root';
+    $pass = 'root';
+    $charset = 'utf8';
+
+    $dsn = "mysql:host=$host;$charset=$charset";
+    $opt = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+    ];
+
+    $pdo = new PDO($dsn, $user, $pass, $opt);
+
+    $pdo->exec("CREATE DATABASE IF NOT EXISTS `$db`;
+                CREATE USER '$user'@'localhost' IDENTIFIED BY '$pass';
+                GRANT ALL ON `$db`.* TO '$user'@'localhost';
+                FLUSH PRIVILEGES;")
+    or die(print_r($pdo->errorInfo(), true));
+
+    $pdo->query("use `validation-form`");
+    $pdo->exec("CREATE TABLE IF NOT EXISTS `information` (
+                        `id` INT AUTO_INCREMENT NOT NULL,
+                        `address1` varchar(100),
+                        `address2` varchar(100),
+                        `city` varchar(100),
+                        `state` varchar(100),
+                        `zip_code` INT NOT NULL,
+                        PRIMARY KEY (`id`))
+                        CHARACTER SET utf8 COLLATE utf8_general_ci");
+
+    $dataSave = [
+        'address1' => $data['address_1'], 'address2' => $data['address_2'], 'city' => $data['city'], 'state' => $data['state'], 'zip_code' => $data['zip-code']
+    ];
+    $sql = "INSERT INTO information (address1, address2, city, state, zip_code) VALUES (:address1, :address2, :city, :state, :zip_code)";
+    $stmt= $pdo->prepare($sql);
+    $stmt->execute($dataSave);
 }
--- main.js
+++ main.js
@@ -1,28 +1,80 @@
+var modal = document.querySelector(".modal");
+var container = modal.querySelector(".container");
+let data = new FormData();
+
+let address_1 = document.querySelector('#address-line-1').value
+let address_2 = document.querySelector('#address-line-2').value
+let city = document.querySelector('#city').value
+let state = document.querySelector('#state').value
+let zip_code = document.querySelector('#zip-code').value
+
+data.append('address_1', address_1);
+data.append('address_2', address_2);
+data.append('city', city);
+data.append('state', state);
+data.append('zip-code', zip_code);
+
 document.querySelector('#send').addEventListener('click', function (e) {
     e.preventDefault()
 
-    let address_1 = document.querySelector('#address-line-1').value
-    let address_2 = document.querySelector('#address-line-2').value
-    let city = document.querySelector('#city').value
-    let state = document.querySelector('#state').value
-    let zip_code = document.querySelector('#zip-code').value
-
     let xhr = new XMLHttpRequest();
     let url = 'backend.php';
-    let data = new FormData();
-
-    data.append('address_1', address_1);
-    data.append('address_2', address_2);
-    data.append('city', city);
-    data.append('state', state);
-    data.append('zip-code', zip_code);
 
     xhr.open('POST', url, true);
-    // xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
     xhr.onreadystatechange = function() {//Call a function when the state changes.
         if(xhr.readyState === 4 && xhr.status === 200) {
-            alert(xhr.responseText);
+            modal.classList.remove("hidden")
+            let matchError = new RegExp(/error/)
+            //matchError.test(xhr.responseText)
+
+            if (matchError.test(xhr.responseText)) {
+                document.querySelector('#error').innerHTML = xhr.responseText
+                document.querySelector('#save').classList.add('d-none')
+                data.delete('save_data');
+            } else {
+                document.querySelector('#address-1').innerHTML = address_1;
+                document.querySelector('#address-2').innerHTML = address_2;
+                document.querySelector('#city-1').innerHTML = city;
+                document.querySelector('#state-1').innerHTML = state;
+                document.querySelector('#zip-code-1').innerHTML = zip_code;
+                document.querySelector('#save').classList.remove('d-none')
+            }
+        }
+    }
+    xhr.send(data);
+})
+
+document.querySelector("#close").addEventListener("click", function (e) {
+    modal.classList.add("hidden");
+});
+
+document.querySelector('#save').addEventListener('click', function (e) {
+    e.preventDefault()
+
+    data.append('save_data', true);
+
+    let xhr = new XMLHttpRequest();
+    let url = 'backend.php';
+
+    xhr.open('POST', url, true);
+
+    xhr.onreadystatechange = function() {//Call a function when the state changes.
+        if(xhr.readyState === 4 && xhr.status === 200) {
+            modal.classList.remove("hidden")
+
+            let matchError = new RegExp(/error/)
+            if (matchError.test(xhr.responseText)) {
+                document.querySelector('#error').innerHTML = xhr.responseText
+                document.querySelector('#save').classList.add('d-none')
+            } else {
+                document.querySelector('#address-1').innerHTML = address_1;
+                document.querySelector('#address-2').innerHTML = address_2;
+                document.querySelector('#city-1').innerHTML = city;
+                document.querySelector('#state-1').innerHTML = state;
+                document.querySelector('#zip-code-1').innerHTML = zip_code;
+                document.querySelector('#save').classList.remove('d-none')
+            }
         }
     }
     xhr.send(data);
